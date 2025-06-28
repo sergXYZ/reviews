440b040580d7478fbef9b58ce8769aac2ee71bd5
sergXYZ <serhiis1993@gmail.com>
2025-06-28 23:53:55 +0200

    add logic

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,31 @@ import { FaQuoteRight } from 'react-icons/fa6'
 const App = () => {
   const [index, setIndex] = useState(0)
   const { id, name, job, image, text } = person[index]
-  console.log(person[index])
+
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1
+
+      return newIndex % person.length
+    })
+  }
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1 + person.length
+
+      return newIndex % person.length
+    })
+  }
+  const randomPerson = () => {
+    let randomIndex = Math.floor(Math.random() * person.length)
+    if (randomIndex === index) {
+      randomIndex++
+    }
+    const newIndex = randomIndex % person.length
+    setIndex(newIndex)
+  }
+
+  // console.log(person[index])
   return (
     <main>
       <article>
@@ -15,13 +39,14 @@ const App = () => {
         <h4>{job}</h4>
         <p>{text}</p>
         <div>
-          <button type="button">
+          <button type="button" onClick={prevPerson}>
             <FaChevronLeft />
           </button>
-          <button type="button">
+          <button type="button" onClick={nextPerson}>
             <FaChevronRight />
           </button>
         </div>
+        <button onClick={randomPerson}>Random person</button>
       </article>
     </main>
   )
